6a657b80f03f94866bd5a05e86dbcba019488212
Yifang Ma <yifang@Agota.localdomain>
2019-07-03 13:15:21 -0700

    clean up the code

--- assembly/main.ts
+++ assembly/main.ts
@@ -4,9 +4,179 @@ export { memory };
 import { context, storage, near, collections, base64 } from "./near";
 import { Corgi, CorgiArray, CorgiMetaData } from "./model.near";
 
+export {CorgiTokenMarket} ;
+
 // --- contract code goes below
 ///////////////////////////////
 // ERC721 for Non Fungible Tokens
+
+class CorgiTokenMarket {
+  // Constant Terms for Corgi
+  NAME: string = "The NEAR Corgi Token";
+  SYMBOL: string = "CORG"
+  TOTAL_SUPPLY: u64 = 420;
+  DNA_DIGITS: u32 = 16;
+  HEX_ALPHABET = '0123456789abcdef';
+
+  //Methods to call for Terms 
+  name(): string {
+    return NAME;
+  }
+
+  symbol(): string {
+    return SYMBOL;
+  }
+
+  // Initialization
+  init(initialOwner: string): void {
+    near.log("initialOwner: " + initialOwner);
+    assert(storage.getItem("init") == null, "Already initialized token supply");
+    storage.setU64("io::" + initialOwner, TOTAL_SUPPLY);
+    storage.setItem("init", "done");
+  }
+
+  // Collections where we store data
+  balances = collections.map<string, u64>("b");
+  corgis = collections.map<string, Corgi>("c");
+  corgisByOwner = collections.map<string, CorgiArray>("co");
+
+  // Total Supply for user
+  totalSupply(): string {
+    return TOTAL_SUPPLY.toString();
+  }
+
+  //Balance for owner
+  // Owner ID
+  ownerOf(tokenId: string): string {
+    let corgi = getCorgi(tokenId);
+    let owner = corgi.owner;
+    return owner;
+  }
+  // Balance for owner
+  balanceOf(owner: string): u64 {
+    return balances.get(owner);
+  }
+
+  setBalance(owner: string, balance: u64): void {
+    balances.set(owner, balance);
+  }
+
+  updateBalance(owner: string, increment: u64): void {
+    let balance = balanceOf(owner);
+    if (balance) {
+      near.log(`${balance}`);
+    } else {
+      near.log("issues");
+    }
+  }
+
+  //Actions for Corgi
+
+  transfer(to: string, tokenId: string): void {
+    let corgi = getCorgi(tokenId);
+    assert(corgi.owner !== context.sender, "corgi does not belong to " + context.sender);
+    incrementNewOwnerCorgis(to, tokenId);
+    decrementOldOwnerCorgis(corgi.owner, tokenId);
+  }
+  
+  incrementNewOwnerCorgis(to: string, tokenId: string): void {
+    let corgi = getCorgi(tokenId);
+    corgi.owner = to;
+    near.log(to);
+    setCorgisByOwner(corgi);
+    setCorgi(corgi);
+  }
+
+  decrementOldOwnerCorgis(from: string, tokenId: string): void {
+    let _corgis = corgisByOwner.get(from).corgis;
+    for (let i = 0; i < _corgis.length; i++) {
+      if (tokenId == _corgis[i].dna) {
+        _corgis.splice(i, 1);
+        near.log("match");
+      }
+    }
+    let oca = new CorgiArray()
+    oca.corgis = _corgis;
+    corgisByOwner.set(from, oca);
+  }
+
+  //Create Corgi
+
+  _createCorgi(name: string, dna: string, color: string): Corgi {
+    let corgi = new Corgi();
+    corgi.owner = context.sender;
+    corgi.dna = dna;
+    corgi.name = name;
+    corgi.color = color
+    setCorgi(corgi);
+    setCorgisByOwner(corgi);
+    return corgi;
+  }
+
+  createRandomCorgi(name: string, seed: i32): Corgi {
+    let randDna = generateRandomDna();
+    let color = generateRandomColorHex(seed);
+    return _createCorgi(name, randDna, color);
+  }
+
+  // Generate unique Corgi
+  generateRandomDna(): string {
+    let buf = near.randomBuffer(DNA_DIGITS);
+    let b64 = base64.encode(buf);
+    near.log(b64);
+    return b64;
+  }
+
+  generateRandomColorHex(int: i32): string {
+    Math.seedRandom(int);
+    let rand = Math.floor(Math.random() * 16777215) as u32;
+    let hex = intToHex(rand);
+    return "#" + hex;
+  }
+  
+  getCorgi(tokenId: string): Corgi {
+    return corgis.get(tokenId);
+  }
+  
+  setCorgi(corgi: Corgi): void {
+    corgis.set(corgi.dna, corgi);
+  }
+  
+  getSender(): string {
+    return context.sender;
+  }
+  
+  getCorgisByOwner(owner: string): CorgiArray {
+    return corgisByOwner.get(owner);
+  }
+
+  setCorgisByOwner(corgi: Corgi): void {
+    let _corgis = getCorgisByOwner(corgi.owner).corgis;
+  
+    if (_corgis == null) {
+      _corgis = new Array();
+      _corgis.push(corgi);
+    } else {
+      _corgis.push(corgi);
+    }
+  
+    let ca = new CorgiArray();
+  
+    ca.corgis = _corgis;
+    corgisByOwner.set(corgi.owner, ca);
+  }
+
+  
+  
+  //ERROR handling
+  _corgiDNEError(corgi: Corgi): bool {
+    return assert(corgi == null, "This corgi does not exist");
+  }
+
+}
+
+
+
 const NAME: string = "The NEAR Corgi Token";
 const SYMBOL: string = "CORG"
 const TOTAL_SUPPLY: u64 = 420;
@@ -25,8 +195,8 @@ let balances = collections.map<string, u64>("b");
 let corgis = collections.map<string, Corgi>("c");
 let corgisByOwner = collections.map<string, CorgiArray>("co");
 
-export function login():void {
-}
+// export function login():void {
+// }
 
 export function balanceOf(owner: string): u64 {
   return balances.get(owner);
@@ -50,14 +220,14 @@ export function symbol(): string {
   return SYMBOL;
 }
 
-export function transfer(to: string, tokenId: string):void {
+export function transfer(to: string, tokenId: string): void {
   let corgi = getCorgi(tokenId);
   assert(corgi.owner !== context.sender, "corgi does not belong to " + context.sender);
   incrementNewOwnerCorgis(to, tokenId);
   decrementOldOwnerCorgis(corgi.owner, tokenId);
 }
 
-function incrementNewOwnerCorgis(to: string, tokenId: string):void {
+function incrementNewOwnerCorgis(to: string, tokenId: string): void {
   let corgi = getCorgi(tokenId);
   corgi.owner = to;
   near.log(to);
@@ -65,7 +235,7 @@ function incrementNewOwnerCorgis(to: string, tokenId: string):void {
   setCorgi(corgi);
 }
 
-function decrementOldOwnerCorgis(from: string, tokenId: string):void {
+function decrementOldOwnerCorgis(from: string, tokenId: string): void {
   let _corgis = corgisByOwner.get(from).corgis;
   for (let i = 0; i < _corgis.length; i++) {
     if (tokenId == _corgis[i].dna) {
@@ -87,7 +257,7 @@ function updateBalance(owner: string, increment: u64): void {
   }
 }
 
-function _createCorgi(name: string, dna: string, color:string): Corgi {
+function _createCorgi(name: string, dna: string, color: string): Corgi {
   let corgi = new Corgi();
   corgi.owner = context.sender;
   corgi.dna = dna;
@@ -116,11 +286,11 @@ function intToHex(integer: u32): string {
   return hex.length % 2 ? "0" + hex : hex;
 }
 
-export function generateRandomColorHex(int:i32):string {
+export function generateRandomColorHex(int: i32): string {
   Math.seedRandom(int);
   let rand = Math.floor(Math.random() * 16777215) as u32;
   let hex = intToHex(rand);
-  return "#"+hex;
+  return "#" + hex;
 }
 
 export function createRandomCorgi(name: string, seed: i32): Corgi {
