c1a22e029a280f340901dc777c37d24ab28791fe
Yifang Ma <yifang@nearprotocol.com>
2019-07-18 14:44:53 -0700

    setup wallet creation

--- src/main.js
+++ src/main.js
@@ -7,22 +7,28 @@ import "./config.js"
 
 async function doInitContract() {
   // window.near = await nearlib.dev.connect(nearConfig);
-  nearConfig.nodeUrl = 'https://rpc.nearprotocol.com';
-  nearConfig.helperUrl = 'https://near-contract-helper.onrender.com';
+  // nearConfig.nodeUrl = 'https://rpc.nearprotocol.com';
+  // nearConfig.helperUrl = 'https://near-contract-helper.onrender.com';
   console.log("nearConfig", nearConfig);
+  // const walletBaseUrl = 'https://wallet.nearprotocol.com';
+  // window.walletAccount = new nearlib.WalletAccount(nearConfig.contractName, walletBaseUrl);
 
-  const walletBaseUrl = 'https://wallet.nearprotocol.com';
-  window.walletAccount = new nearlib.WalletAccount(nearConfig.contractName, walletBaseUrl);
+  // New version for nearlib
+  // Initializing connection to the NEAR node.
+  window.near = await nearlib.connect(Object.assign(nearConfig, { deps: { keyStore: new nearlib.keyStores.BrowserLocalStorageKeyStore() } }));
+  // Needed to access wallet login
+  window.walletAccount = new nearlib.WalletAccount(window.near);
+
 
   // Getting the Account ID. If unauthorized yet, it's just empty string.
   window.accountId = window.walletAccount.getAccountId();
 
   // Initializing near and near client from the nearlib.
-  window.near = new nearlib.Near(new nearlib.NearClient(
-    window.walletAccount,
-    // We need to provide a connection to the blockchain node which we're going to use
-    new nearlib.LocalNodeConnection(nearConfig.nodeUrl),
-  ));
+  // window.near = new nearlib.Near(new nearlib.NearClient(
+  //   window.walletAccount,
+  //   // We need to provide a connection to the blockchain node which we're going to use
+  //   new nearlib.LocalNodeConnection(nearConfig.nodeUrl),
+  // ));
 
   window.contract = await near.loadContract(nearConfig.contractName, {
     viewMethods: [
@@ -55,8 +61,8 @@ function sleep(time) {
   });
 }
 
-window.nearInitPromise = doInitContract().then(()=>{
-  ReactDOM.render( <App contract={contract} wallet={walletAccount} /> ,
+window.nearInitPromise = doInitContract().then(() => {
+  ReactDOM.render(<App contract={contract} wallet={walletAccount} />,
     document.getElementById('container')
   );
 }).catch(console.error)
