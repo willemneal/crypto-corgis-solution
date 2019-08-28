a62be41ead95d8fcae0fc445606f1040f52eebb3
Yifang Ma <yifang@nearprotocol.com>
2019-08-28 11:39:43 -0700

    change load image method in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,28 @@ module.exports = {
   // context: path.resolve(__dirname, "/src"),
   module: {
     rules: [{
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]",
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['react-hot-loader/webpack', 'babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /config\.js$/,
-        use: ['script-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {},
-          },
-        ],
-      },
+      test: /\.html$/,
+      loader: "file?name=[name].[ext]",
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['react-hot-loader/webpack', 'babel-loader']
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /config\.js$/,
+      use: ['script-loader']
+    },
+    {
+      test: /\.(png|jpe?g|gif)$/,
+      use: [
+        'file-loader'
+      ],
+    },
     ]
   },
   output: {
